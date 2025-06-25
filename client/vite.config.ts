31320c6a186dc826ff163617b5777b245ecc7ed3
aravindkumarchittem <163434731+aravindkumarchittem@users.noreply.github.com>
2025-06-25 11:41:24 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/client/', // <-- Ensure this is correct
+  base: '/', // <-- Ensure this is correct
   plugins: [react()]
 })
