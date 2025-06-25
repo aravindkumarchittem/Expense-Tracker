b1e1e07b4b5ecd919dff2a0ca4076ade78b71699
aravindkumarchittem <163434731+aravindkumarchittem@users.noreply.github.com>
2025-06-25 15:43:48 +0530

    Update expenseRoutes.js

--- backend/api/expenseRoutes.js
+++ backend/api/expenseRoutes.js
@@ -1,5 +1,5 @@
 
-const express = require('express');
+const express = require('express').Router();
 const Expense = require('../models/Expense');
 const router = express.Router();
 // const { authenticate } = require('../middlewares/authMiddleware');
